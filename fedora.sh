ff24aa0acc71c70f034dcfb5f353272db07afc79
evseib <43419956+evseib@users.noreply.github.com>
2023-05-13 00:28:20 -0300

    Add files via upload

--- fedora.sh
+++ fedora.sh
@@ -14,47 +14,33 @@ sudo fwupdmgr refresh --force
 sudo fwupdmgr get-updates 
 sudo fwupdmgr update
 
-sudo dnf install -y @base-x gnome-shell gnome-terminal nautilus firefox
+sudo dnf install -y xorg-x11-server-Xwayland libxcb egl-wayland gnome-shell gnome-terminal nautilus xdg-user-dirs xdg-user-dirs-gtk 
 sudo systemctl set-default graphical.target
-sudo dnf install -y gnome-software net-tools chrome-gnome-shell gnome-tweaks @development-tools gnome-extensions-app
 
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf group -y install "Hardware Support"
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install -y gnome-software net-tools gnome-tweaks @development-tools gnome-extensions-app
+sudo dnf install -y vlc steam btop gnome-system-monitor file-roller firefox gnome-terminal-nautilus gparted ffmpegthumbnailer 
 
-sudo dnf install vlc steam btop gnome-shell-extensions gnome-shell-extensions-user-theme gnome-system-monitor file-roller 
+sudo dnf install -y gstreamer1-plugins-{bad-*,good-*,ugly-*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
+sudo dnf -y groupupdate sound-and-video
 
-sudo dnf group install "Hardware Support"
-sudo dnf install gnome-terminal-nautilus gparted xdg-user-dirs xdg-user-dirs-gtk ffmpegthumbnailer
-
-sudo dnf install gstreamer1-plugins-{bad-*,good-*,ugly-*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
-sudo dnf  groupupdate sound-and-video
 sudo dnf config-manager --set-enabled fedora-cisco-openh264
-sudo dnf install  gstreamer1-plugin-openh264 mozilla-openh264
-sudo dnf install fira-code-fonts 'mozilla-fira*' 'google-roboto*'
+sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
+sudo dnf install -y fira-code-fonts 'mozilla-fira*' 'google-roboto*'
+sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig
 
-
-sudo dnf install curl cabextract xorg-x11-font-utils fontconfig
 sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-sudo dnf install  unzip p7zip p7zip-plugins unrar 
-sudo dnf install qbittorrent 
-sudo dnf install dnf-plugins-core
-sudo dnf install fedora-workstation-repositories
+sudo dnf install -y unzip p7zip p7zip-plugins unrar qbittorrent dnf-plugins-core fedora-workstation-repositories
 
 sudo dnf copr enable peterwu/rendezvous
 sudo dnf install -y bibata-cursor-themes
 
 
 
-
-
-
-
-
 #NVIDIA wayland
 #verificar se é wayland ou x11
 echo $XDG_SESSION_TYPE
-
-
-dnf install xorg-x11-server-Xwayland libxcb egl-wayland
 
 # edit grub /etc/default/grub
 ## Example row with Fedora 38/37/36 BTRFS ##
@@ -74,13 +60,13 @@ ENV{NVIDIA_PRESERVE_VIDEO_MEMORY_ALLOCATIONS}!=“1”, GOTO=“gdm_disable_wayl
 
 # generate initramfs
 ## Backup old initramfs nvidia-nomodeset image ##
-mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nvidia-nomodeset.img
+sudo mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nvidia-nomodeset.img
  
 ## Create new initramfs image ##
 dracut /boot/initramfs-$(uname -r).img $(uname -r)
 
 # precisa sair r voltar  ao terminal
-# e executar como usuário normal para verificar se 'kms-modifiers' está ativo
+# e executar como usuário normal para verificar se 'kms-modifiers' está ativo -> responde apenas "@as []"
 gsettings get org.gnome.mutter experimental-features
 # então executar
 gsettings set org.gnome.mutter experimental-features [\"kms-modifiers\"]
