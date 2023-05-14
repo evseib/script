fa595c3358005a9fe9952d01afc22910efabe8c1
evseib <43419956+evseib@users.noreply.github.com>
2023-05-14 14:35:43 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -1,23 +1,18 @@
-#cat /etc/dnf/dnf.conf
-#[main]
-#gpgcheck=1
-#installonly_limit=3
-#clean_requirements_on_remove=True
-#best=False
-#skip_if_unavailable=True
-#max_parallel_downloads=10
-#fastestmirror=True
-#defaultyes=True
+#cat /etc/dnf/dnf.conf 
+sudo echo "max_parallel_downloads=20" >> /etc/dnf/dnf.conf 
+sudo echo "fastestmirror=True" >> /etc/dnf/dnf.conf 
+sudo echo "defaultyes=True" >> /etc/dnf/dnf.conf 
 
-sudo fwupdmgr get-devices 
-sudo fwupdmgr refresh --force 
-sudo fwupdmgr get-updates 
-sudo fwupdmgr update
+# sudo fwupdmgr get-devices 
+# sudo fwupdmgr refresh --force 
+# sudo fwupdmgr get-updates 
+# sudo fwupdmgr update
+
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf install -y xorg-x11-server-Xwayland libxcb egl-wayland gnome-shell gnome-terminal nautilus xdg-user-dirs xdg-user-dirs-gtk 
 sudo systemctl set-default graphical.target
 
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf group -y install "Hardware Support"
 
 sudo dnf install -y gnome-software net-tools gnome-tweaks @development-tools gnome-extensions-app
@@ -57,52 +52,94 @@ sudo dnf install -y bibata-cursor-themes
 
 
 # instalar NVIDIA
-# https://www.if-not-true-then-false.com/2015/fedora-nvidia-guide/
+# sudo dnf -y install kmod-nvidia
+
+
+
+
+
+
+
+sudo dnf update -y # and reboot if you are not on the latest kernel
+sudo dnf install akmod-nvidia # rhel/centos users can use kmod-nvidia instead
+sudo dnf install xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
+
+
+
+#sudo grubby --update-kernel=ALL --args='nvidia-drm.modeset=1'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #NVIDIA wayland
 #verificar se é wayland ou x11
-echo $XDG_SESSION_TYPE
+#echo $XDG_SESSION_TYPE
 
 # edit grub /etc/default/grub
 ## Example row with Fedora 38/37/36 BTRFS ##
-GRUB_CMDLINE_LINUX="rhgb quiet rd.driver.blacklist=nouveau nvidia-drm.modeset=1"
+#GRUB_CMDLINE_LINUX="rhgb quiet rd.driver.blacklist=nouveau nvidia-drm.modeset=1"
 ## OR with LVM ##
-GRUB_CMDLINE_LINUX="rd.lvm.lv=fedora/swap rd.lvm.lv=fedora/root rhgb quiet rd.driver.blacklist=nouveau nvidia-drm.modeset=1"
+#GRUB_CMDLINE_LINUX="rd.lvm.lv=fedora/swap rd.lvm.lv=fedora/root rhgb quiet rd.driver.blacklist=nouveau nvidia-drm.modeset=1"
 
 
 ## BIOS and UEFI ##
-grub2-mkconfig -o /boot/grub2/grub.cfg
+#grub2-mkconfig -o /boot/grub2/grub.cfg
 
 
 #Update /usr/lib/udev/rules.d/61-gdm.rules on Fedora 38/37/36
 #Comment line 
-ENV{NVIDIA_PRESERVE_VIDEO_MEMORY_ALLOCATIONS}!=“1”, GOTO=“gdm_disable_wayland”
+#ENV{NVIDIA_PRESERVE_VIDEO_MEMORY_ALLOCATIONS}!=“1”, GOTO=“gdm_disable_wayland”
 
 
 # generate initramfs
 ## Backup old initramfs nvidia-nomodeset image ##
-sudo mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nvidia-nomodeset.img
+#sudo mv /boot/initramfs-$(uname -r).img /boot/initramfs-$(uname -r)-nvidia-nomodeset.img
  
 ## Create new initramfs image ##
-dracut /boot/initramfs-$(uname -r).img $(uname -r)
+#dracut /boot/initramfs-$(uname -r).img $(uname -r)
 
 # precisa sair r voltar  ao terminal
 # e executar como usuário normal para verificar se 'kms-modifiers' está ativo -> responde apenas "@as []"
-gsettings get org.gnome.mutter experimental-features
+#gsettings get org.gnome.mutter experimental-features
 # então executar
-gsettings set org.gnome.mutter experimental-features [\"kms-modifiers\"]
+#gsettings set org.gnome.mutter experimental-features [\"kms-modifiers\"]
 
 
 
-reboot
+#reboot
 
 # to change hostname
-nano /etc/hostname
+#nano /etc/hostname
 # type the desired name and done
 
 # check fully updated installation
-lsb_release -a 
+#lsb_release -a 
 # yes pra tudo
 
 
@@ -110,10 +147,10 @@ lsb_release -a
 
 
 # para verificar
-nvidia-installer -v 
-echo $XDG_SESSION_TYPE #deve retornar 'wayland'
+#nvidia-installer -v 
+#echo $XDG_SESSION_TYPE #deve retornar 'wayland'
 
-watch -n1 nvidia-smi
+#watch -n1 nvidia-smi
 # e verificar se processes tem programas que estão usando memoria gpu
 # movimentar janelas deve aumentar gpu usage 
 
