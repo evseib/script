b22906afa0d6bfca77f9033df6fa2c03e98077e2
evseib <43419956+evseib@users.noreply.github.com>
2023-05-14 18:04:05 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -10,6 +10,7 @@ sudo echo "defaultyes=True" >> /etc/dnf/dnf.conf
 
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
+
 sudo dnf install -y xorg-x11-server-Xwayland libxcb egl-wayland gnome-shell gnome-terminal nautilus xdg-user-dirs xdg-user-dirs-gtk 
 sudo systemctl set-default graphical.target
 
