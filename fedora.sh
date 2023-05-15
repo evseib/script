b463301d756ec5004bcb7c4238d23a65403fb91c
evseib <43419956+evseib@users.noreply.github.com>
2023-05-14 21:56:36 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -12,25 +12,18 @@ sudo echo "alias 'll=ls -la --color=auto'" >> ~/.bashrc
 
 sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-
 sudo dnf install -y xorg-x11-server-Xwayland libxcb egl-wayland gnome-shell gnome-terminal nautilus xdg-user-dirs xdg-user-dirs-gtk 
 sudo systemctl set-default graphical.target
 
 sudo dnf group -y install "Hardware Support"
+sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+sudo dnf config-manager --set-enabled fedora-cisco-openh264
 
-sudo dnf install -y gnome-software net-tools gnome-tweaks @development-tools gnome-extensions-app
-sudo dnf install -y vlc steam btop gnome-system-monitor file-roller firefox gnome-terminal-nautilus gparted ffmpegthumbnailer gnome-browser-connector
-
+sudo dnf install -y gnome-software net-tools gnome-tweaks @development-tools gnome-extensions-app vlc steam btop gnome-system-monitor file-roller firefox gnome-terminal-nautilus gparted ffmpegthumbnailer gnome-browser-connector
 sudo dnf install -y gstreamer1-plugins-{bad-*,good-*,ugly-*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
 sudo dnf -y groupupdate sound-and-video
 
-sudo dnf config-manager --set-enabled fedora-cisco-openh264
-sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
-sudo dnf install -y fira-code-fonts 'mozilla-fira*' 'google-roboto*' 
-sudo dnf install -y curl cabextract xorg-x11-font-utils fontconfig kitty
-
-sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
-sudo dnf install -y unzip p7zip p7zip-plugins unrar qbittorrent dnf-plugins-core fedora-workstation-repositories papirus-icon-theme gnome-weather
+sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264 fira-code-fonts 'mozilla-fira*' 'google-roboto*' curl cabextract xorg-x11-font-utils fontconfig kitty unzip p7zip p7zip-plugins unrar qbittorrent dnf-plugins-core fedora-workstation-repositories papirus-icon-theme hydrapaper 
 
 sudo dnf copr enable peterwu/rendezvous
 sudo dnf install -y bibata-cursor-themes
@@ -42,23 +35,23 @@ sudo dnf install -y bibata-cursor-themes
 # na pasta ~/.themes instalar os arquivos -> descomprimir com tar xf arquivo
 # https://www.pling.com/p/1357889?ref=itsfoss.com
 
+# talvez - testar
+# sudo dnf install dnf5 dnf5-plugins
 
 
 # https://github.com/vinceliuice/Tela-icon-theme
 # 
 
 
-
 # https://extensions.gnome.org/extension/2890/tray-icons-reloaded
 # https://extensions.gnome.org/extension/5090/space-bar
-
+# https://extensions.gnome.org/extension/5446/quick-settings-tweaker/
 
 
 # instalar NVIDIA
 
 sudo dnf update -y # and reboot if you are not on the latest kernel
-sudo dnf install akmod-nvidia # rhel/centos users can use kmod-nvidia instead
-sudo dnf install xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
+sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda 
 
 
 
