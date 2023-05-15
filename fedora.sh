a9045d695e046686fcb559b9822cd9b169f19ef9
evseib <43419956+evseib@users.noreply.github.com>
2023-05-14 23:12:35 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -50,7 +50,7 @@ sudo dnf install -y bibata-cursor-themes
 
 # instalar NVIDIA
 
-sudo dnf update -y # and reboot if you are not on the latest kernel
+# sudo dnf update -y # and reboot if you are not on the latest kernel
 sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda 
 
 
